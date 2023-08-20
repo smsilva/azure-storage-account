bad15d871226f649c8b88a3d84aea455b247ca05
Silvio Silva <smsilva@gmail.com>
2023-08-20 17:08:33 -0300

    feat(src/main.tf): Update parameter for public access

--- src/main.tf
+++ src/main.tf
@@ -3,13 +3,13 @@ locals {
 }
 
 resource "azurerm_storage_account" "default" {
-  name                      = local.storage_account_name
-  resource_group_name       = var.resource_group.name
-  location                  = var.resource_group.location
-  account_tier              = var.account_tier
-  account_replication_type  = var.account_replication_type
-  allow_blob_public_access  = var.allow_blob_public_access
-  min_tls_version           = "TLS1_2"
-  enable_https_traffic_only = true
-  tags                      = var.tags
+  name                          = local.storage_account_name
+  resource_group_name           = var.resource_group.name
+  location                      = var.resource_group.location
+  account_tier                  = var.account_tier
+  account_replication_type      = var.account_replication_type
+  public_network_access_enabled = var.allow_blob_public_access
+  min_tls_version               = "TLS1_2"
+  enable_https_traffic_only     = true
+  tags                          = var.tags
 }
